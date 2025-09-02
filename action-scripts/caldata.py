59a459a8a28f4c465ac513b7605c9102016d387b
bri-ne <cervantesblb@gmail.com>
2025-09-02 00:52:11 -0400

    trying

--- action-scripts/caldata.py
+++ action-scripts/caldata.py
@@ -3,6 +3,7 @@ import os
 import datetime as dt
 import json
 key = os.environ["key"]
+print(key)
 calid = '9srf5u5iffu2dg06625hbvhbj8@group.calendar.google.com'
 timemin = str(dt.datetime.now().date())
 
@@ -10,6 +11,8 @@ timemin = str(dt.datetime.now().date())
 r = f'https://www.googleapis.com/calendar/v3/calendars/{calid}/events?orderBy=startTime&showDeleted=false&singleEvents=true&timeMin={timemin}T00%3A00%3A00Z&key={key}'
 t = requests.get(r)
 data = t.json()
+print(r)
 
+print(t)
 with open('output.json', 'w') as f:
     json.dump(data, f)
