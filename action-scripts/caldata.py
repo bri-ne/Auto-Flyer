c347b05e2954a39fb84a7d4ba20057d6a9a6d21c
bri-ne <cervantesblb@gmail.com>
2025-09-02 00:18:44 -0400

    cleanup

---
+++ action-scripts/caldata.py
@@ -0,0 +1,15 @@
+import requests
+import os 
+import datetime as dt
+import json
+key = os.environ["key"]
+calid = '9srf5u5iffu2dg06625hbvhbj8@group.calendar.google.com'
+timemin = str(dt.datetime.now().date())
+
+
+r = f'https://www.googleapis.com/calendar/v3/calendars/{calid}/events?orderBy=startTime&showDeleted=false&singleEvents=true&timeMin={timemin}T00%3A00%3A00Z&key={key}'
+t = requests.get(r)
+data = t.json()
+
+with open('output.json', 'w') as f:
+    json.dump(data["items"], f)
