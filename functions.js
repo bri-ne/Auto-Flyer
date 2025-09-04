03abc247b6207f53992925d4dd53683025b18fef
bri-ne <cervantesblb@gmail.com>
2025-09-04 01:40:25 -0400

    mkaing it work again

--- functions.js
+++ functions.js
@@ -35,50 +35,51 @@ function getEvents() {
   })
 }
 getEvents();
-//== Variables ==//
+//== letiables ==//
 const subtitle = document.querySelector(".subtitle")
 let caldata
-var subMo = [];
+let subMo = [];
 
 
 
 
 //== Funcations ==//
 function addEvent(edate, mo, etitle, etime, elocation, elink) {
-  var div_content = document.createElement('div');
-  var h_edate = `<div class="eventdate"><div class="date-day">${edate}</div><div class="date-mo">${mo}</div></div>`
-  var h_etitle = `<div class="event-title">${etitle}</div>`
-  var h_etime = `<div class="event-time">${etime}</div>`
-  var h_elocation = `<div class="event-location">${elocation}</div>`
-  //var h_elink = `<div class="event-link">${elink}</div></div>`
-  div.className = 'container';
-  div.innerHTML = h_edate + '<div class="calevent">' + h_etitle + h_etime + h_elocation + /*h_elink +*/ '</div>';
+  let div_content = document.createElement('div');
+  let h_edate = `<div class="eventdate"><div class="date-day">${edate}</div><div class="date-mo">${mo}</div></div>`
+  let h_etitle = `<div class="event-title">${etitle}</div>`
+  let h_etime = `<div class="event-time">${etime}</div>`
+
+  let h_elocation 
+  if (elocation === undefined){
+    h_elocation =  '<div class="event-location">TBD</div>'
+  }else{
+    h_elocation =  `<div class="event-location">${elocation}</div>`
+  }
+  //let h_elink = `<div class="event-link">${elink}</div></div>`
+  div_content.className = 'container edit';
+  div_content.contentEditable = 'False';
+  div_content.innerHTML = h_edate + '<div class="calevent">' + h_etitle + h_etime + h_elocation + /*h_elink +*/ '</div>';
   document.getElementById("calcontent").appendChild(div_content);
 }
 
 function showEvents(d) {
-  let cal_s
-  let cal_e
-  let edate
-  let mo 
-  let etitle
-  let etime
-  let elocation
   let sub
+  let upcoming = caldata.filter((item) => new Date(item.start.dateTime) > d );
   /* this will hold the for loop on addEvent*/
   /*first filter the caldata to only the next 5 events based on the date filter*/
   for (let k = 0; k <6; k++) {
     console.log(k)
-    cal_s = new Date(caldata[k].start.dateTime);
-    cal_e = new Date(caldata[k].end.dateTime);
+    let cal_s = new Date(upcoming[k].start.dateTime);
+    let cal_e = new Date(upcoming[k].end.dateTime);
     if (cal_s < d) {
-      console.log(`${caldata[k].summary} is not in time range`)
-    } else {edate = cal_s.getDate();
-      mo = cal_s.toLocaleString('default', { month: 'long' });
-      etitle = caldata[k].summary;
-      etime = cal_s.toLocaleTimeString() + " - " + cal_e.toLocaleTimeString() // will need to do some formatting here
-      elocation = caldata[k].location
-      //let elink = caldata[k][whatever]
+      console.log(`${upcoming[k].summary} is not in time range`)
+    } else {let edate = cal_s.getDate();
+      let mo = cal_s.toLocaleString('default', { month: 'long' });
+      let etitle = upcoming[k].summary;
+      let etime = cal_s.toLocaleTimeString() + " - " + cal_e.toLocaleTimeString() // will need to do some formatting here
+      let elocation = upcoming[k].location
+      //let elink = upcoming[k][whatever]
       subMo.push(mo)
       addEvent(edate, mo, etitle, etime, elocation);//, elink);
   };};
@@ -88,10 +89,15 @@ function showEvents(d) {
   } else {
     sub = subMo[0] + ' - ' + subMo[4];
   };
-  subtitle.innerHTML += sub;
+  // if heading is undefined send error
+  if (sub === undefined) {
+    subtitle.innerHTML += 'no events found :/';
+  } else {
+    subtitle.innerHTML += sub;
+  }
+  
 
 }
-
 
 
 
@@ -100,9 +106,10 @@ function showEvents(d) {
 
 document.getElementById("dateInput").addEventListener("input", event => {
   document.getElementById("calcontent").innerHTML = '';
-  //var input = this.value;
-  var dateEntered = new Date(event.target.value)//.toISOString();
-  //var v = new Date(input);
+  subtitle.innerHTML = '';
+  //let input = this.value;
+  let dateEntered = new Date(event.target.value)//.toISOString();
+  //let v = new Date(input);
   // console.log(input); //e.g. 2015-11-13
   console.log(event);
   console.log(dateEntered); //e.g. Fri Nov 13 2015 00:00:00 GMT+0000 (GMT Standard Time)
@@ -110,6 +117,42 @@ document.getElementById("dateInput").addEventListener("input", event => {
   showEvents(dateEntered);
  ;
 });
+
+
+//make cal details editable
+
+
+
+const toggleButton = document.querySelector('#editToggle');
+let editElems = document.getElementsByClassName("edit")
+
+function butttog() {
+  if (editElems[0].contentEditable==='false') {
+    for(let j = 0; j < editElems.length; j++){
+      editElems[j].contentEditable="True";
+    };
+    console.log('Button is active!');
+    toggleButton.innerHTML = 'Edit ON'
+    alert('Look at you editing ;) dont forget to the update the calendar!')
+    // Insert on logic here
+  }else{
+    alert('Edit is off')
+    for(let j = 0; j < editElems.length; j++){
+      editElems[j].contentEditable="False";
+    }
+    console.log('Button is not active.');
+    toggleButton.innerHTML = 'Edit OFF'
+    // Insert off logic here
+  }
+
+};
+
+
+// Add an event listener for the 'click' event
+toggleButton.addEventListener('click', function() {
+  butttog();
+});
+
 
 
 
