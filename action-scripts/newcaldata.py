f53c2c53db8b3656c25c6a3ca0e57a303c3ee354
bri-ne <cervantesblb@gmail.com>
2026-03-28 02:17:33 -0400

    newwcacl

--- action-scripts/newcaldata.py
+++ action-scripts/newcaldata.py
@@ -6,6 +6,6 @@ with open('output.json', 'w') as f:
     json.dump(jicson.fromFile('./calendar.ics'), f)
 f.close()
 
-calendar_string = ic.convert_ics_to_string('./calendar.ics')
+calendar_string = ic.convert_ics_to_string('../calendar.ics')
 event_list = ic.make_event_list(calendar_string)
 ic.convert_list_to_csv(event_list, "calendar.csv")
